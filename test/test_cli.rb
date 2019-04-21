f544035aec01069ec8e6f81dd31e9ba9539987c6
Adam Daniels <adam@mediadrive.ca>
2019-04-21 18:06:47 +0000

    Test CLI

---
+++ test/test_cli.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require "fileutils"
+require "open3"
+
+class TestCli < Minitest::Test
+  def test_build
+    build_destination = Dir.mktmpdir("inert")
+
+    # TODO: Specify build destination option to CLI
+    exe = File.expand_path("../exe/inert", __dir__)
+    lib = File.expand_path("../lib", __dir__)
+
+    Dir.chdir(File.expand_path("example", __dir__)) do
+      output, status = Open3.capture2e("ruby -I#{lib} #{exe} build")
+      assert_equal "Saving /", output.strip
+      assert_equal 0, status.exitstatus
+    end
+  ensure
+    FileUtils.remove_entry(build_destination) if build_destination
+  end
+end
